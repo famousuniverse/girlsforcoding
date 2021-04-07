14a096dc70ce27096ae2705e0984d060b2c3fe69
RR <rramirez@medinformatix.com>
2021-04-06 20:26:56 -0700

    new changes colors and the way or functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'girlsforcoding';
+
+
+  constructor(
+  ) {}
+
+  ngOnInit() {  
+    console.log('hello Colette');
+
+}
+// alert('red');
+
+//this is a function or a way, a method of doing something
+// the way 1
+coletteFavoriteColor(){
+  console.log('asdfasdfasdfasdfasdfsdf');
+  alert('helloColette blue');
+}
+
+
+// the way 2
+rileysFavoriteColor(){
+  alert('helloRiley green');
+}
+
+
 }
